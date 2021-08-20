5f9121af9af21138da3d9892cf8a6d5b08ae4921
GiverofMemory <78986433+GiverofMemory@users.noreply.github.com>
2021-08-20 01:44:33 -0700

    Update config.php
    change directory for uploads based on web host folder directory format.

--- wiki/local/config.php
+++ wiki/local/config.php
@@ -46,7 +46,7 @@ $UploadPermAdd = 0;
 $DefaultPasswords['upload'] ='$2y$10$YMReOid8MK7GDTyhc1Z.TeKfZLGIMNhnXig3Ah0k1vaKsZCyER.jO';
 $UploadDirQuota = 100000000; # limit total uploads to 100000KB (100MB)
 $UploadMaxSize = 52000; # limit each upload to 52KB
-$UploadDir = "/home/naturevault/public_html/wiki/uploads";
+$UploadDir = "/naturevault/home/public/wiki/uploads";
 $UploadUrlFmt = "https://www.naturevault.org/wiki/uploads";
 $LinkUploadCreateFmt = "<a rel='nofollow' class='createlinktext' href='\$LinkUpload'>\$LinkText</a>";
 
