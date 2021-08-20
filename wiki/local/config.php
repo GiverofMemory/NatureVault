2c1a2866a618ce55ba9ab1d1cf13b0a4af85ca7b
GiverofMemory <78986433+GiverofMemory@users.noreply.github.com>
2021-08-20 03:07:59 -0700

    Update config.php
    adding farmd seeing if it messes up site dump

--- wiki/local/config.php
+++ wiki/local/config.php
@@ -13,10 +13,10 @@
 ########### Cookbooks###########
 
 ##Site Dump##
-include_once("cookbook/sitedump.php");
+include_once("$FarmD/cookbook/sitedump.php");
 
 ##Section Edit##
-include_once("cookbook/sectionedit.php");
+include_once("$FarmD/cookbook/sectionedit.php");
 $SectionEditLinkText = "";
 $SectionEditHeaderLinkSpan = 1;
 $SectionEditWithoutHeaders = false;
