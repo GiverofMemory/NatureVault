367f6c2264c96f03e49b39ff0edb7373c055c050
GiverofMemory <78986433+GiverofMemory@users.noreply.github.com>
2021-08-26 13:40:41 -0700

    Update config.php

--- wiki/local/config.php
+++ wiki/local/config.php
@@ -69,10 +69,11 @@ $EditTemplatesFmt = '{$Group}.Template';
 include_once("$FarmD/scripts/refcount.php");
 
 ########## Privacy ############
-$EnableRevUserAgent = 1;  ## 1 means enable logging of users browser info when making a revision or post
-$EnableRevHostIP = 1;     ## 1 means enable logging of users IP Address when making a revision or post
-$EnablePostUserAgent = 1; ## 1 means enable logging of users browser info when making a new post
-$EnablePostHostIP = 1;    ## 1 means enable logging of users IP Address when making a new post
+## These don't seem to work (nothing is logged) with any setting see pmwiki.php for the lines.
+$EnableRevUserAgent = 0;  ## 1 means enable logging of users browser info when making a revision or post
+$EnableRevHostIP = 0;     ## 1 means enable logging of users IP Address when making a revision or post
+$EnablePostUserAgent = 0; ## 1 means enable logging of users browser info when making a new post
+$EnablePostHostIP = 0;    ## 1 means enable logging of users IP Address when making a new post
 
 ####################
 ##End custom lines##
