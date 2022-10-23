d53f0a19967bcd8e2eeaf8272898100f0497d99f
GiverofMemory <78986433+GiverofMemory@users.noreply.github.com>
2022-10-23 17:05:47 -0400

    improving

--- wiki/cookbook/titledictindexn.php
+++ wiki/cookbook/titledictindexn.php
@@ -10,12 +10,12 @@
     By default the items are display as a simple definition list, but
     this can be controlled by $FPLDictIndex...Fmt variables:
 
-        $FPLDictIndexStartFmt   - start string
-        $FPLDictIndexEndFmt     - end string
-        $FPLDictIndexLFmt       - string to output for each new letter
-        $FPLDictIndexIFmt       - string to output for each item in list
-        $FPLDictIndexLEndFmt	- string to output at end of each letter block
-        $FPLDictIndexHeaderLink - string for the link list at the upper page
+        $IndexnStartFmt   - start string
+        $IndexnEndFmt     - end string
+        $IndexnLFmt       - string to output for each new letter
+        $IndexnIFmt       - string to output for each item in list
+        $IndexnLEndFmt	- string to output at end of each letter block
+        $IndexnHeaderLink - string for the link list at the upper page
 
     To enable this module, simply add the following to config.php:
 
@@ -23,30 +23,28 @@
 
 */
 
-$FPLFunctions['dictindexn'] = 'FPLDictIndex';
-global $DictIndexShowLetterLinksByDefault;
-SDV($DictIndexShowLetterLinksByDefault, true);
-SDV($DictIndexNoLetters, true);
+$FPLFunctions['dictindexn'] = 'Indexn';
+global $DictIndexShowLetterLinksByDefaultn;
+SDV($DictIndexShowLetterLinksByDefaultn, true);
 
-function FPLDictIndex($pagename,&$matches,$opt) {
-global $FPLDictIndexStartFmt, 
-	$FPLDictIndexEndFmt,
-	$FPLDictIndexLFmt,
-	$FPLDictIndexIFmt,
-	$FPLDictIndexLEndFmt,
-	$FPLDictIndexHeaderLink,
-	$DictIndexShowLetterLinksByDefault,
-	$DictIndexNoLetters,
+function Indexn($pagename,&$matches,$opt) {
+global $IndexnStartFmt, 
+	$IndexnEndFmt,
+	$IndexnLFmt,
+	$IndexnIFmt,
+	$IndexnLEndFmt,
+	$IndexnHeaderLink,
+	$DictIndexShowLetterLinksByDefaultn,
 	$FmtV;
 	
 	$opt['order']='title';
 	$matches = MakePageList($pagename, $opt);
-	SDV($FPLDictIndexStartFmt,"<dl class='fpldictindex'>\n");
-	SDV($FPLDictIndexEndFmt,'</dl>');
-	SDV($FPLDictIndexLFmt,"<dt><a href='#dictindexheader' id='\$IndexLetter'>&#9650;</a> \$IndexLetter</dt>\n");
-	SDV($FPLDictIndexLEndFmt,"");
-	SDV($FPLDictIndexIFmt,"<dd><a href='\$PageUrl' title='\$Group : \$Title'>\$Title</a></dd>\n");
-	SDV($FPLDictIndexHeaderLink,"\n".'<a href="#$IndexLetter">$IndexLetter</a>');
+	SDV($IndexnStartFmt,"<dl class='fpldictindex'>\n");
+	SDV($IndexnEndFmt,'</dl>');
+	SDV($IndexnLFmt,"<dt><a href='#dictindexheader' id='\$IndexLetter'>&#9650;</a> \$IndexLetter</dt>\n");
+	SDV($IndexnLEndFmt,"");
+	SDV($IndexnIFmt,"<dd><a href='\$PageUrl' title='\$Group : \$Title'>\$Title</a></dd>\n");
+	SDV($IndexnHeaderLink,"\n".'<a href="#$IndexLetter">$IndexLetter</a>');
 
 	$out = array();
 	$headerlinks= array();
@@ -54,23 +52,23 @@ global $FPLDictIndexStartFmt,
 		#$pletter = substr($item['=title'],0,1);
 		$FmtV['$IndexLetter'] = $pletter;
 		if (strcasecmp($pletter,@$lletter)!=0) {
-			if($lletter) { $out[] = FmtPageName($FPLDictIndexLEndFmt,$item['pagename']); }
-			$out[] = FmtPageName($FPLDictIndexLFmt,$item['pagename']);
-			$headerlinks[] = FmtPageName($FPLDictIndexHeaderLink,$item['pagename']);
+			if($lletter) { $out[] = FmtPageName($IndexnLEndFmt,$item['pagename']); }
+			$out[] = FmtPageName($IndexnLFmt,$item['pagename']);
+			$headerlinks[] = FmtPageName($IndexnHeaderLink,$item['pagename']);
 			$lletter = $pletter; 
 		}
-		$out[] = FmtPageName($FPLDictIndexIFmt,$item['pagename']);
+		$out[] = FmtPageName($IndexnIFmt,$item['pagename']);
 	}
-	if(!empty($headerlinks)) { $out[] = FmtPageName($FPLDictIndexLEndFmt,$item['pagename']); }
+	if(!empty($headerlinks)) { $out[] = FmtPageName($IndexnLEndFmt,$item['pagename']); }
 	$FmtV['$IndexLinks']=implode(' &bull; ',$headerlinks);
 	
-	$show_letter_links = isset($opt['letterlinks']) ? $opt['letterlinks'] : $DictIndexShowLetterLinksByDefault;
+	$show_letter_links = isset($opt['letterlinks']) ? $opt['letterlinks'] : $DictIndexShowLetterLinksByDefaultn;
 
 	return 
 		FmtPageName(($show_letter_links ? "<p id='dictindexheader'>\$IndexLinks</p><hr>" : ""),$pagename) . 
-		FmtPageName($FPLDictIndexStartFmt,$pagename) . 
+		FmtPageName($IndexnStartFmt,$pagename) . 
 		implode('',$out) . 
-		FmtPageName($FPLDictIndexEndFmt,$pagename);
+		FmtPageName($IndexnEndFmt,$pagename);
 }
 
 ?>
